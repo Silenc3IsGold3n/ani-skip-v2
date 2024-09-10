f965f93a354011b6d503e21076649d22af194aa2
Silenc3IsGold3n <7948133+Silenc3IsGold3n@users.noreply.github.com>
2024-09-10 18:42:24 -0400

    Update skip.lua
    add recap skipping to skip.lua

--- skip.lua
+++ skip.lua
@@ -3,14 +3,14 @@ local mpv = require('mp')
 local mpv_options = require("mp.options")
 
 local options = { -- setting default options
-    op_start = 0, op_end = 0, ed_start = 0, ed_end = 0,
+    op_start = 0, op_end = 0, ed_start = 0, ed_end = 0, recap_start = 0, recap_end = 0,
 }
 mpv_options.read_options(options, "skip") --reading script-opts data
 
 -- Main function to check and skip if within the defined section
 local function skip()
     local current_time = mp.get_property_number("time-pos")
-    
+
     if not current_time then
         return
     end
@@ -19,10 +19,15 @@ local function skip()
     if current_time >= options.op_start and current_time < options.op_end then
         mp.set_property_number("time-pos", options.op_end)
     end
-    
+
     -- Check for ending sequence
     if current_time >= options.ed_start and current_time < options.ed_end then
         mp.set_property_number("time-pos", options.ed_end)
+    end
+
+    -- Check for ending sequence
+    if current_time >= options.recap_start and current_time < options.recap_end then
+        mp.set_property_number("time-pos", options.recap_end)
     end
 end
 
