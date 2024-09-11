c96fe7c77b54fb5ad3644db63854d1bf50dd415e
Silenc3IsGold3n <7948133+Silenc3IsGold3n@users.noreply.github.com>
2024-09-11 19:54:48 -0400

    Update skip.lua
    implement https://github.com/synacktraa/ani-skip/pull/8#issue-2241679483 changes

--- skip.lua
+++ skip.lua
@@ -2,6 +2,9 @@
 local mpv = require('mp')
 local mpv_options = require("mp.options")
 
+--Default states
+local enabled = true
+local keybind = "i"
 local options = { -- setting default options
     op_start = 0, op_end = 0, ed_start = 0, ed_end = 0, recap_start = 0, recap_end = 0,
 }
@@ -12,6 +15,10 @@ local function skip()
     local current_time = mp.get_property_number("time-pos")
 
     if not current_time then
+        return
+    end
+
+    if not enabled then  
         return
     end
 
@@ -31,5 +38,10 @@ local function skip()
     end
 end
 
+local function enabled_state()
+    enabled = not enabled
+    mp.osd_message("Ani-Skip is " .. (enabled and "enabled" or "disabled"), 1)
+end  
 -- Bind the function to be called whenever the time position is changed
 mp.observe_property("time-pos", "number", skip)
+mp.add_key_binding(keybind, "skip_enable", enabled_state)
